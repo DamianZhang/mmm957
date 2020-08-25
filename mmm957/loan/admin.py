2fe8c44a48e995577c5597e98b422f579e39a33f
DamianZhang <54428512+DamianZhang@users.noreply.github.com>
2020-08-25 14:41:58 +0800

    Push the first version

---
+++ mmm957/loan/admin.py
@@ -0,0 +1,30 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+
+from .models import MyUser, Borrower, Borrowing_Message, Price, Employee, Lender, Paying, Ad_Contact, Ad
+
+
+# user:mmm957, password:ntustntust
+
+class MyUserInline(admin.StackedInline):
+    model = MyUser
+    can_delete = False
+    verbose_name_plural = "MyUser"
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (MyUserInline,)
+
+
+# Register your models here.
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.register(Borrower)
+admin.site.register(Borrowing_Message)
+admin.site.register(Price)
+admin.site.register(Employee)
+admin.site.register(Lender)
+admin.site.register(Paying)
+admin.site.register(Ad_Contact)
+admin.site.register(Ad)
